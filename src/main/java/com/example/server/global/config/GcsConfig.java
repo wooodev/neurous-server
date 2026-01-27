ac18e377ad3d23e82863abd072bb25b66b072704
wooodev <142153611+wooodev@users.noreply.github.com>
2026-01-27 23:57:14 +0900

    #74 fix: ci 테스트에서 GCS 빈 생성 제외

--- src/main/java/com/example/server/global/config/GcsConfig.java
+++ src/main/java/com/example/server/global/config/GcsConfig.java
@@ -5,11 +5,13 @@ import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
+@Profile("!test")
 public class GcsConfig {
 
     @Bean
